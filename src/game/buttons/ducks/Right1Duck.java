8e050d467e8a8b1ed125eea0cb74623f1297a33e
Vitahu <79806896+VitahuHere@users.noreply.github.com>
2022-05-09 09:14:42 +0200

    Update Right1Duck.java

--- src/game/buttons/ducks/Right1Duck.java
+++ src/game/buttons/ducks/Right1Duck.java
@@ -2,7 +2,7 @@ package game.buttons.ducks;
 
 public class Right1Duck extends Duck0 {
     public Right1Duck() {
-        super("src/game/images/right 1 duck.png");
+        super("src/game/images/ducks/right 1 duck.png");
         this.movementSpeed = 3;
         this.lives = 1;
     }
